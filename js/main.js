94cc8a77adfb4867188d3bd95c470cd173e88b62
Edgaras <noredgaras@gmail.com>
2018-11-12 14:32:27 +0200

    format

--- js/main.js
+++ js/main.js
@@ -1,48 +1,44 @@
 class TextAreaSettings {
-    constructor(minRows, maxRows, id) {
+    constructor(minRows, maxRows, id){
         this.minRows = minRows;
         this.maxRows = maxRows;
-        this.id = id;        
-    }  
+        this.id = id;
+    }
 }
 
-let defaultSettings = new TextAreaSettings(10,20,"content");
+var defaultSettings = new TextAreaSettings(10, 20, "content");
 
 function handleChange() {    
-    
     updateTextArea(defaultSettings);
 }
 
-function updateTextArea(defaultSettings){
-    let textRows = getTextRowsCount(defaultSettings.id);    
-    
-    if(isTextAreaEmpty(defaultSettings.id))
-    {        
-        setRows(defaultSettings.id,defaultSettings.minRows);        
-    }else if(isTextRowsExceedsGivenRows(textRows,defaultSettings.minRows))
-    {
-        setRows(defaultSettings.id,defaultSettings.maxRows);        
-    }else if(isTextRowsExceedsGivenRows(textRows,defaultSettings.maxRows))
-    {
+function updateTextArea(defaultSettings) {    
+    var textRows = getTextRowsCount(defaultSettings.id);
+
+    if (isTextAreaEmpty(defaultSettings.id)) {
+        setRows(defaultSettings.id, defaultSettings.minRows);
+    } else if (isTextRowsExceedsGivenRows(textRows, defaultSettings.minRows)) {
+        setRows(defaultSettings.id, defaultSettings.maxRows);
+    } else if (isTextRowsExceedsGivenRows(textRows, defaultSettings.maxRows)) {
         setScrollBar(defaultSettings.id);
     }
 }
 
-function isTextRowsExceedsGivenRows(textRows, givenRows){
- return textRows > givenRows
+function isTextRowsExceedsGivenRows(textRows, givenRows) {    
+    return textRows > givenRows;
 }
 
-function isTextAreaEmpty(textAreaId){
-    return document.getElementById(textAreaId).value == ''
+function isTextAreaEmpty(textAreaId) {    
+    return document.getElementById(textAreaId).value == '';
 }
-function getTextRowsCount(textAreaId){
+function getTextRowsCount(textAreaId) {    
     return document.getElementById(textAreaId).value.split("\n").length;
 }
 
-function setRows(textAreaId, rows){
+function setRows(textAreaId, rows) {
     document.getElementById(textAreaId).rows = rows;
 }
 
-function setScrollBar(textAreaId){
+function setScrollBar(textAreaId) {
     document.getElementById(textAreaId).style.overflowY = "scroll";
 }
